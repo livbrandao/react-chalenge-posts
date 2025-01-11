05563c9fb348744035e95cce8f806b984a556d3f
Livia Brandao <liviatoledo27@gmail.com>
2025-01-11 20:31:49 -0300

    adds the functionality of scrolling to the top of the page when clicking on “Edit”

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,9 @@
 const PostItem = ({ post, onEdit, onDelete }) => {
+  const handleEdit = (post) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    onEdit(post);
+  };
+
   return (
     <div className="p-4 bg-gray-50 border rounded-lg shadow-sm">
       <h2 className="text-xl font-bold text-gray-800">{post.title}</h2>
@@ -7,7 +12,7 @@ const PostItem = ({ post, onEdit, onDelete }) => {
       </p>
       <div className="mt-4 flex space-x-2">
         <button
-          onClick={() => onEdit(post)}
+          onClick={() => handleEdit(post)}
           className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 text-sm sm:text-base md:text-lg"
         >
           Editar
